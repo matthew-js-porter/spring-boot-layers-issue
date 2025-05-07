7f38f184b80f2758404f8e4006edf68021fe65bc
matthew-js-porter <matthew.js.porter@jpmchase.com>
2025-05-07 09:13:50 -0400

    sample issue

---
+++ demo-rest/src/main/java/com/example/demo_layers_issue/DemoLayersIssueApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_layers_issue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLayersIssueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLayersIssueApplication.class, args);
+	}
+
+}
---
+++ demo-rest/src/test/java/com/example/demo_layers_issue/DemoLayersIssueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_layers_issue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLayersIssueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
